55915d77c0a499b0a163fc19ed3a64cd89598c81
Wanjiku <wanjikumwaura0304@gmail.com>
2025-06-21 12:42:52 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = (num1, num2) => num1/num2
+console.log(divide(2000, 100))
+
+        // two
+const square = num1 => num1*num1        
+console.log(square(2))
+
+        // Three
+ const add =(num1, num2) => num1 + num2
+ console.log(add(10,10))     
